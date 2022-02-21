b558a00f58d113cd3bd68e085517f4ae7c2d23a6
Angel Valdez <ang_endless27@hotmail.com>
2022-02-20 18:34:16 -0600

    first

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.home')
+
+@section('title', 'Login')
+
+@section('content')
+
+<div>
+    <div class="grid gap-4 my-24 place-content-center h-48 border-2 border-gray-900">
+        <h1>Login</h1>
+    </div>
+</div>
+
+
+
+@endsection
